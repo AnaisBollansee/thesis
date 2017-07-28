7458f9c0048b4fd2908dc13e94279f7fb2d2b30f
Pablo <pablo.bollansee@gmail.com>
2017-07-28 17:43:56 +0200

    Printable long-read

--- js/start_long_read.js
+++ js/start_long_read.js
@@ -1,26 +1,3 @@
 $(document).ready(function() {
-    var sections = []
-    $('.section').each(function(index, value) {
-        var attr = $(this).attr('anchor');
-        if (typeof attr !== typeof undefined && attr !== false) {
-            sections.push($( this ).attr('anchor'))
-        }
-        else {
-            sections.push("" + (index+1))
-        }
-    })
-    console.log(sections)
-
-    /*$.each(sections, function(index, value) {
-        $('#menu').append('<li data-menuanchor="' + value + '"><a href="#' + value + '">' + value + '</a></li>')
-    })*/
-
-    $('#fullpage').fullpage({
-        //menu: '#menu',
-        anchors: sections,
-        navigation: true,
-        navigationPosition: 'right',
-        showActiveTooltip: true
-        //navigationTooltips: sections
-    });
+    $('.section').slice(1).before('<hr>')
 });
